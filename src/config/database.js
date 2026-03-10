3b03d66961ed56c7b8a9a09d03ec8ac755fbd917
RidyaG <ridyagupta@gmail.com>
2026-03-10 23:53:04 +0530

    update docker and railway deployment config

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,9 @@
-// src/config/database.js
-
 const { PrismaClient } = require('@prisma/client');
 const { PrismaPg } = require('@prisma/adapter-pg');
+const { Pool } = require('pg');
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
 
 const prisma = new PrismaClient({
   adapter,
