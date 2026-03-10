7e2ddeb8390420e5b739aeaba3a228325b66ac63
RidyaG <ridyagupta@gmail.com>
2026-03-11 02:04:38 +0530

    script commit: 1PERCENT backend

--- scripts/migrate.js
+++ scripts/migrate.js
@@ -9,5 +9,3 @@ execSync('npx prisma migrate deploy', {
     DATABASE_URL: migrateUrl
   }
 });
-
-DATABASE_URL_EXTERNAL = "postgresql://postgresql_nq2k_user:pCWz2FGP26mFCxVWeG8nsaKSo7hxxbK6@dpg-d6o74a2a214c73f3urqg-a.oregon-postgres.render.com/postgresql_nq2k"
