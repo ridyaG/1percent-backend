075856365fb4075328afa9a6a9eb670a30c31869
RidyaG <ridyagupta@gmail.com>
2026-03-11 22:51:29 +0530

    PostRoutes

--- src/modules/posts/postController.js
+++ src/modules/posts/postController.js
@@ -7,6 +7,12 @@ exports.createPost = async (req, res, next) => {
         authorId: req.user.id,
         content: req.body.content,
         postType: req.body.postType || "daily_win"
+      },
+      include: {
+        author: {
+          select: { id: true, username: true, displayName: true, avatarUrl: true }
+        },
+        _count: { select: { likes: true, comments: true } }
       }
     });
 
@@ -18,9 +24,17 @@ exports.createPost = async (req, res, next) => {
 
 exports.getFeed = async (req, res, next) => {
   try {
+    const limit = parseInt(req.query.limit) || 20;
+    
     const posts = await prisma.post.findMany({
       orderBy: { createdAt: "desc" },
-      take: 20
+      take: limit,
+      include: {
+        author: {
+          select: { id: true, username: true, displayName: true, avatarUrl: true }
+        },
+        _count: { select: { likes: true, comments: true } }
+      }
     });
 
     res.json({ success: true, data: posts });
